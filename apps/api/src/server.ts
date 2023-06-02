4319f9391a475c93d3d6c40cf7c32ffe9593c683
Thada Wangthammang <thada.wth@gmail.com>
2023-06-02 21:05:12 +0700

    add zod

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -6,7 +6,6 @@ import cors from "cors";
 export const createServer = () => {
   const app = express();
   app
-    .disable("x-powered-by")
     .use(morgan("dev"))
     .use(urlencoded({ extended: true }))
     .use(json())
